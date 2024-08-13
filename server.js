23bedaeda6c5844492fb42bed08affcce86035bc
ZAKUZ? <92181137+creazyspam@users.noreply.github.com>
2024-08-14 01:00:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
-const port = 3000; // You can use any available port
+const port = 3009; // You can use any available port
 
 app.use(cors()); // Enable CORS for requests from frontend
 app.use(express.json()); // Parse JSON bodies
 
 // MySQL connection setup
 const connection = mysql.createConnection({
-    host: 'your-database-host',
-    user: 'your-database-user',
-    password: 'your-database-password',
-    database: 'your-database-name'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'githubsite'
 });
 
 connection.connect(err => {
