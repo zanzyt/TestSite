456507bbd90fc466635b14409e08f4d83cc986e4
zakusz <92181137+zanzyt@users.noreply.github.com>
2025-01-29 13:57:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-<script>
-    fetch('https://api64.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('ip-address').textContent = data.ip;
-        })
-        .catch(error => {
-            document.getElementById('ip-address').textContent = 'Unable to fetch IP';
-        });
-</script>
-
-
 document.getElementById("light-mode-btn").addEventListener("click", function() {
     document.body.classList.remove("dark-mode");
     document.body.classList.add("light-mode");
@@ -19,3 +7,13 @@ document.getElementById("dark-mode-btn").addEventListener("click", function() {
     document.body.classList.remove("light-mode");
     document.body.classList.add("dark-mode");
 });
+
+// Fetch IP Address
+fetch('https://api64.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('ip-address').textContent = data.ip;
+    })
+    .catch(error => {
+        document.getElementById('ip-address').textContent = 'Unable to fetch IP';
+    });
